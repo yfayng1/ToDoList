6a12eb736653a2ef299b7866c8d0a5723c77cbed
Fima Fayngersh <yfayng1@students.towson.edu>
2016-02-29 13:55:00 -0500

    3B: Convert to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160229172003) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "to_dos", force: :cascade do |t|
     t.string   "task"
     t.string   "due_date"
